f29e767ff86001adc9cbda4820314b93292fab18
vrdse <vrdse@users.noreply.github.com>
2018-01-06 21:33:34 +0100

    minor fix

--- MeltdownSpectreReport.ps1
+++ MeltdownSpectreReport.ps1
@@ -1034,7 +1034,7 @@ $GetMeltdownStatusInformation = {
             ($SpeculationControlSettings.KVAShadowWindowsSupportEnabled -eq $true) -and
             ($SpeculationControlSettings.KVAShadowPcidEnabled -eq $true)) {
             $mitigated = $true
-        } # What if KVAShadowPcidEnable -eq false?
+        }
         else {
             $mitigated = $false
         }
@@ -1119,7 +1119,7 @@ $GetMeltdownStatusInformation = {
             # See https://blog.mozilla.org/security/2018/01/03/mitigations-landing-new-class-timing-attack/
             $Firefox = (Get-Item -Path 'C:\Program Files\Mozilla Firefox\firefox.exe', 
                 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe' -ErrorAction SilentlyContinue)
-            $FirefoxVersion = ($Firefox.VersionInfo.ProductVersion | Sort-Object)[0] -as [version]
+            $FirefoxVersion = ($Firefox.VersionInfo.ProductVersion | Sort-Object | Select-Object -First 1) -as [version]
             if ($FirefoxVersion -ge [version]'57.0.4') {
                 $FirefoxMitigated = $true
             }
